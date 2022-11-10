41ed6e489f401f3a5907d52c88be79a049af4408
AgnieszkaRaczynska <agnieszka.raczynska@ymail.com>
2022-11-10 17:57:50 +0100

    project upload

---
+++ weatherapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=d3fee0091d9ff3db0128230e0d1ca77d').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code":str(json_data['sys']['country']),
+            "coordinate":str(json_data['coord']['lon'])+', '+str(json_data['coord']['lat']),
+            "temp":str(json_data['main']['temp'])+' K (Kelvin)',
+            "pressure":str(json_data['main']['pressure'])+' hPa',
+            "clouds":str(json_data['weather'][0]['description']),
+            "wind":str(json_data['wind']['speed'])+' M/H',
+        }
+
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city': city, 'data':data})
+
